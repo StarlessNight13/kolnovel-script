021aeeabf943903ab38205d2ec7aa2c13ce93ca6
darklessnight <darklessnight13@proton.me>
2025-03-18 14:31:50 +0100

    updateURL

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite';
-import monkey from 'vite-plugin-monkey';
+import { defineConfig } from "vite";
+import monkey from "vite-plugin-monkey";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     monkey({
-      entry: 'src/main.ts',
+      entry: "src/main.ts",
+      server: {
+        open: false,
+      },
       userscript: {
-        icon: 'https://vitejs.dev/logo.svg',
-        namespace: 'npm/vite-plugin-monkey',
-        match: ['https://kolbook.xyz/*'],
+        icon: "https://vitejs.dev/logo.svg",
+        namespace: "npm/vite-plugin-monkey",
+        match: ["https://kolbook.xyz/*"],
+        updateURL:
+          "https://github.com/StarlessNight13/kolnovel-script/releases/latest/download/kolnovel-script.user.js",
+        downloadURL:
+          "https://github.com/StarlessNight13/kolnovel-script/releases/latest/download/kolnovel-script.user.js",
       },
     }),
   ],
