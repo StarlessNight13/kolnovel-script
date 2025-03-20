6c976e8bc34f958ad71404bbb4763b44337c6580
darklessnight <darklessnight13@proton.me>
2025-03-20 19:02:19 +0100

    fix updateURL

--- vite.config.ts
+++ vite.config.ts
@@ -13,12 +13,13 @@ export default defineConfig({
       userscript: {
         icon: "https://www.google.com/s2/favicons?sz=64&domain=kolbook.xyz",
         namespace: "darkless/kolnovel",
+        author: "Darkless",
         version: "1.0.1",
         match: ["https://kolbook.xyz/*", "https://kolnovel.com/*"],
         updateURL:
-          "https://github.com/StarlessNight13/kolnovel-script/releases/latest/download/kolnovel-script.user.js",
+          "https://raw.githubusercontent.com/StarlessNight13/kolnovel-script/releases/latest/download/kolnovel-script.user.js",
         downloadURL:
-          "https://github.com/StarlessNight13/kolnovel-script/releases/latest/download/kolnovel-script.user.js",
+          "https://raw.githubusercontent.com/StarlessNight13/kolnovel-script/releases/latest/download/kolnovel-script.user.js",
       },
     }),
   ],
