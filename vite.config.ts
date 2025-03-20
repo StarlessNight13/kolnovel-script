c74e1b81a0255290fa51364cfc11deb50b5c749d
darklessnight <darklessnight13@proton.me>
2025-03-20 16:09:09 +0100

    setup version and namespace and icon

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,9 @@ export default defineConfig({
         open: false,
       },
       userscript: {
-        icon: "https://vitejs.dev/logo.svg",
-        namespace: "npm/vite-plugin-monkey",
+        icon: "https://www.google.com/s2/favicons?sz=64&domain=kolbook.xyz",
+        namespace: "darkless/kolnovel",
+        version: "1.0.0",
         match: ["https://kolbook.xyz/*"],
         updateURL:
           "https://github.com/StarlessNight13/kolnovel-script/releases/latest/download/kolnovel-script.user.js",
